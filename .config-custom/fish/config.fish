244ff900091af3bd09ac4a25b1284b0150ffa904
ezhang7423 <ezhang7423@student.palomar.edu>
2021-12-17 12:12:04 -0800

    update config

--- .config-custom/fish/config.fish
+++ .config-custom/fish/config.fish
@@ -1,18 +1,21 @@
-# alaises
+# aliases
 alias c 'xclip -selection clipboard'
 alias start 'xdg-open'
 alias l 'ls -hal'
 alias hal 'ls -hal'
 alias v 'source venv/bin/activate.fish'
+alias rl 'source ~/.rl/bin/activate.fish'
+alias run_py 'nodemon -x "python3 main.py" -e py'
+alias f 'cd $FIRST_PATH'
 
 # variables
 export EDITOR='/usr/bin/vim'
-set PATH   ~/.local/bin  ~/.personalbin  ~/.cargo/bin ~/flutter/bin ~/.npm-global/bin $PATH
+set -x -p PATH ~/.local/bin ~/.personalbin ~/.cargo/bin ~/flutter/bin ~/.npm-global/bin
+set FIRST_PATH (pwd -P)
 
 # function fish_prompt
 #     history -a
 #     history -r
 # end
 
-alias v 'source venv/bin/activate.fish'
 source ~/.asdf/asdf.fish
